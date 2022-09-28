2ea1059fb748f12f5b18fd34f2d6bb9a1c39115e
“Amin <73975219+code-amin@users.noreply.github.com>
2022-09-28 11:33:42 -0400

    Initial commit - template

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+
+express()
+  .use(morgan("tiny"))
+
+  .get("/hello", (req, res) => {
+    res.status(200).json({ status: 200, messagE: "hi" });
+  })
+
+  .listen(2000, () => {
+    console.log(`Example app listening on port 2000`);
+  });
